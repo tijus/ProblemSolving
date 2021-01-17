8e0fb83aec1302b9e913ae978817345a1c43028b
Sujit Singh <sujitsingh@Sujits-MacBook-Pro.local>
2021-01-17 13:02:24 -0500

    added TopolocialSort implementation

--- LeetCode/src/algorithm/TopologicalSort.java
+++ LeetCode/src/algorithm/TopologicalSort.java
@@ -1,9 +1,37 @@
 package algorithm;
 
-import kotlin.Pair;
+
 import resource.GraphNode;
+
 
 import java.util.*;
 
 public class TopologicalSort {
+
+    List<GraphNode> ans = new ArrayList<>();
+    Set<GraphNode> visited = new HashSet<>();
+    Stack<GraphNode> sortOrderedStack = new Stack<>();
+
+    public List<GraphNode> topologicalSort(GraphNode root) {
+        dfs(root);
+
+        while (!sortOrderedStack.isEmpty()) {
+            ans.add(sortOrderedStack.pop());
+        }
+
+        return ans;
+    }
+
+    private void dfs(GraphNode node) {
+        if (visited.contains(node))
+            return;
+
+        visited.add(node);
+
+        for (GraphNode child: node.children) {
+            dfs(child);
+        }
+
+        ans.add(node);
+    }
 }
--- LeetCode/src/com/company/Solution.java
+++ LeetCode/src/com/company/Solution.java
@@ -8,45 +8,11 @@ import java.util.*;
 
 public class Solution {
 
-    Map<Integer, List<TreeNode>> map;
-
-    // base case = list of one node
-
-    // event case = empty list of nodes
-
-    // everyting else (list of nodes are the children node, once left and once right);
-
-    public List<TreeNode> allPossibleFBT(int n) {
-
-        this.map = new HashMap<>();
-        if (!map.containsKey(n)) {
-            List<TreeNode> listOfPossibilities = new ArrayList<>();
-            if (n == 1) {
-                listOfPossibilities.add(new TreeNode(0));
-            } else if (n % 2 == 1) {
-                for (int x = 0; x<n; x++) {
-                    int y = n - x - 1;
-                    for (TreeNode left: allPossibleFBT(x)) {
-                        for(TreeNode right: allPossibleFBT(y)) {
-                            TreeNode currNode = new TreeNode(0);
-                            currNode.left = left;
-                            currNode.right = right;
-                            listOfPossibilities.add(currNode);
-                        }
-                    }
-                }
-            }
-            map.put(n, listOfPossibilities);
-        }
-        return map.get(n);
+    public boolean sequenceReconstruction(int[] org, List<List<Integer>> seqs) {
 
     }
 
     public static void main(String[] args) {
-        List<TreeNode> allPossibleFBT = new Solution().allPossibleFBT(7);
 
-        for (TreeNode possibleFBT: allPossibleFBT) {
-            System.out.println(possibleFBT.val);
-        }
     }
 }
