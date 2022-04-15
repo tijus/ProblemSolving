b47338389a8e79438e8262c1dcb6a83afd69fea1
Sujit Singh <sujitsingh@Sujits-MacBook-Pro.local>
2022-04-14 22:19:57 -0700

    code update

--- LeetCode/src/algorithm/graph/Solution.java
+++ LeetCode/src/algorithm/graph/Solution.java
@@ -2,7 +2,16 @@ package algorithm.graph;
 
 public class Solution {
 
-    private boolean isGraphConnected() {
-        return false;
+
+    static String compactAndSort(String rangestring) {
+
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.solution(15958));
+        System.out.println(sol.solution(-5859));
+        System.out.println(sol.solution(5000));
     }
 }
--- LeetCode/src/com/company/Solution.java
+++ LeetCode/src/com/company/Solution.java
@@ -1,9 +1,62 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
 
+    private static void calculateSumOfDigits(int num) {
+        int sumOfDigits = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            num /= 10;
+            sumOfDigits += digit;
+        }
+
+        System.out.println(sumOfDigits);
+    }
+
+    public static void main(String[] args) {
+        // System.in and System.out are input and output streams, respectively.
+        InputStream inputStream = System.in;
+
+        InputReader in = new InputReader(inputStream);
+
+        // Read the number of test casese.
+        int T = in.nextInt();
+        while (T-- > 0) {
+            // Read the numbers a and b.
+            int num = in.nextInt();
+            calculateSumOfDigits(num);
+            // Compute the sum of a and b.
+        }
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
 }
